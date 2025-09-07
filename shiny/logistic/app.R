e15932ae1669acffc3d2e12c3392033212044eb4
Brian Powers <49255223+brianreedpowers@users.noreply.github.com>
2025-09-06 19:56:09 -0500

    edited shiny apps
    removed ggplot from shiny apps - they don't seem to play well with shinylive

--- shiny/logistic/app.R
+++ shiny/logistic/app.R
@@ -16,9 +16,9 @@ ui = fluidPage(
 server = function(input, output) {
   
   library(MASS)
-  library(tidyverse)
-  library(pROC)
-  library(glue)
+#  library(tidyverse)
+#  library(pROC)
+#  library(glue)
   
   if(is.factor(Pima.te$type)) Pima.te$type = ifelse(Pima.te$type=='Yes',1,0)
   model = list(null=glm(type~1, data=Pima.te, family="binomial"),
@@ -26,9 +26,10 @@ server = function(input, output) {
                all=glm(type~., data=Pima.te, family="binomial"))
   
   observeEvent(c(input$threshold,input$model),{
+    predictions = predict.glm(model[[input$model]], newdata=Pima.te, type="response")
     conf.tab = table(
       Pima.te$type,
-      factor(predict.glm(model[[input$model]], type="response") >= input$threshold, levels=c(F,T)))
+      factor(predictions >= input$threshold, levels=c(F,T)))
     colnames(conf.tab) = c("Predict 0","Predict 1")
     rownames(conf.tab) = c("No diabetes","Yes diabetes")
     
@@ -41,27 +42,37 @@ server = function(input, output) {
       TN = conf.tab[1,1]
       FP = conf.tab[1,2]
       FN = conf.tab[2,1]
-      cat("      Accuracy:",round((TP+TN)/(TP+TN+FP+FN),3))
-      cat("\n   Sensitivity:",round(TP/(TP+FN),3))
-      cat("\n   Specificity:",round(TN/(TN+FP),3))
-      cat("\nPrecision(PPV):",round(TP/(TP+FP),3))
-      cat("\n           NPV:", round(TN/(TN+FN),3))
+      cat("           Accuracy:",round((TP+TN)/(TP+TN+FP+FN),3))
+      cat("\n        Sensitivity:",round(TP/(TP+FN),3))
+      cat("\n        Specificity:",round(TN/(TN+FP),3))
+      cat("\nFalse Positive Rate:", round(1- TN/(TN+FP),3))
+      cat("\n     Precision(PPV):",round(TP/(TP+FP),3))
+      cat("\n                NPV:", round(TN/(TN+FN),3))
     })
     
     output$rocPlot = renderPlot({
-      predictions = predict(model[[input$model]], newdata=Pima.te, type="response")
+      predictions = predict.glm(model[[input$model]], newdata=Pima.te, type="response")
+(      thresh=c(0, sort(unique(predictions)),1)
+)      
+      
+      FPR <- thresh; TPR <- thresh
+      for(i in 1:length(thresh)){
+        conf.tab.thresh = table(
+          Pima.te$type,
+          factor(predictions >= thresh[i], levels=c(F,T)))
+        FPR[i] <- conf.tab.thresh[1,2] / sum(conf.tab.thresh[1,])
+        TPR[i] <- conf.tab.thresh[2,2] / sum(conf.tab.thresh[2,])
+      }
+      predAUC = sum(-diff(FPR) * (TPR[-1]+TPR[-length(TPR)])/2)
       pt = data.frame(x=(conf.tab[1,2]/sum(conf.tab[1,])),y=(conf.tab[2,2]/sum(conf.tab[2,])))
-      roc(Pima.te$type~predictions) %>%
-        ggroc(legacy.axes=T) + 
-          labs(title="ROC curve",subtitle=glue("AUC: {round(test_roc$auc,3)}"),
-               x="False positive rate",y="True positive rate") + 
-          scale_x_continuous(expand=c(0,0)) + scale_y_continuous(expand=c(0,0)) + 
-          geom_point(data=pt, aes(x=x, y=y), col="red",size=5) + 
-          geom_abline() + coord_equal()
+      par(pty="s")
+      plot(FPR,TPR, type="l",
+           xlab="false positive rate",
+           ylab="true positive rate",
+           main=paste0("Model: ",input$model,", AUC=",round(predAUC,4)))
+      points(pt$x, pt$y, col="red", pch=16, cex=2)
     })
-    
   })
-  
 }
 
 shinyApp(ui = ui, server = server)
